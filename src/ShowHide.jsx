902adcb1ef67f1d6ac1369d3a285cb1612538629
ShristiSinghAstha <shristisinghastha@gmail.com>
2026-07-06 19:06:00 +0530

    Did some practices

--- src/ShowHide.jsx
+++ src/ShowHide.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 function ShowHide() {
-    const [show, setShow] = useState(true);
+    const [show, setShow] = useState(false);
 
     return (
         <div>
             {show && <h2>Welcome to React</h2>}
-
             <button onClick={()=>setShow(!show)}>
                 {show? "Hide":"Show"}
             </button>
